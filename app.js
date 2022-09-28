219cc095418de2f1f533c8e8c25bc25a0121e2da
Anjil Shrestha <yenjail16@gmail.com>
2022-09-28 10:16:05 +0800

    full pipeline test

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Curtin! we are in review 
-stage!'));
+app.get('/', (req, res) => res.send('Some updated, ready for review stage.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
